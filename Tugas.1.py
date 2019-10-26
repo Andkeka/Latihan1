1488ab1e803ad7dcdab94dc9d505ff2356f29d7c
Andkeka <56200287+Andkeka@users.noreply.github.com>
2019-10-26 17:26:07 +0700

    Add files via upload

---
+++ Tugas.1.py
@@ -0,0 +1,9 @@
+namalengkap = "Andhika Eka Putra"
+namapanggilan = "Dika"
+npm = "311910691"
+tempatlahir = "Bekasi"
+Umur = "21"
+telfon = "0838 0491 7948"
+alamat = "Cikarang - Sukatani"
+
+print("Assalamualaikum, \n let me introduce my self, my name is",namalengkap,"\n but you can call me",namapanggilan,"\n my npm",npm,"\n I was born in",tempatlahir,"\n and I am",Umur,"years old. I very glad if you want to invite my house in",alamat,"\n so don't forget to call me before with the number",telfon,"\n Thank You")
